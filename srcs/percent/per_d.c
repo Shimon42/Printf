7e8e725f7a1244c197a24fafa4bb925d9623f1ac
Siméon F <Shimon42@users.noreply.github.com>
2019-12-04 16:50:23 +0100

    pref 0 for %d

--- srcs/percent/per_d.c
+++ srcs/percent/per_d.c
@@ -6,13 +6,10 @@
 /*   By: siferrar <siferrar@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/20 22:01:41 by siferrar     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/29 22:20:50 by siferrar    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/04 16:43:30 by siferrar    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
-
-#include <stdarg.h>
-#include <stdio.h>
 #include "../../includes/libftprintf.h"
 
 int	per_d(va_list va, t_param *p)
@@ -20,17 +17,21 @@ int	per_d(va_list va, t_param *p)
 	unsigned int	n_print;
 	long			nbr;
 	int				padding;
-	
+	int				i;
+
+	i = 0;
 	n_print = 0;
 	nbr = va_arg(va, int);
 	padding = ft_ilen(nbr) + p->show_sign + ((nbr >= 0) * p->is_sp_pref) + (nbr < 0);
-	n_print += disp_justif(p->min_width - padding, p->left_justif, 0);
-	if (p->show_sign)
+	if (!p->pref_0)
+		n_print += disp_justif(p->min_width - padding, p->left_justif, 0);
+	if (p->show_sign || nbr < 0)
 		(nbr >= 0 ? ft_putchar('+') : ft_putchar('-'));
-	else if (p->is_sp_pref && nbr >= 0)
+	if (p->is_sp_pref && nbr >= 0)
 		n_print += write(1, " ", 1);
-	if (nbr < 0)
-		ft_putchar('-');
+	else if (p->pref_0 && !p->left_justif)
+		while (i++ < p->min_width - padding )
+			n_print += write(1, "0", 1);
 	ft_putnbr_fd(ft_abs(nbr), 1);
 	n_print += ft_ilen(nbr) + (nbr < 0 || p->show_sign);
 	n_print += disp_justif(p->min_width - padding, p->left_justif, 1);
